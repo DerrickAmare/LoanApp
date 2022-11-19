a7bfe49abdd38a1221aa8fba6144d9b019ee0224
DerrickAmare <derrickwejebu21@gmail.com>
2022-11-19 16:47:43 -0600

    second

--- IdeaProjects/Advanced/src/Execeptions/AccountException.java
+++ IdeaProjects/Advanced/src/Execeptions/AccountException.java
@@ -3,5 +3,6 @@ package Execeptions;
 public class AccountException extends Exception {
     public AccountException(Throwable cause) {
         super(cause);
+        System.out.println("hey");
     }
 }
